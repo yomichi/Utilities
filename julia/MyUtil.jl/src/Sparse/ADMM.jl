0477d25519a61b8d02b98fb6c9beb0882f6bc28b
Yuichi Motoyama <yomichi@tsg.jp>
2017-07-12 15:08:46 +0900

    elbow method

--- julia/MyUtil.jl/src/Sparse/ADMM.jl
+++ julia/MyUtil.jl/src/Sparse/ADMM.jl
@@ -8,19 +8,141 @@
     end
 end
 
-export LassoADMM
+@compat mutable struct LassoResult
+    opt_lambda::Float64
+    lambdas::Vector{Float64}
+    residues::Vector{Float64}
+    nonzeros::Vector{Int}
+end
+
+export LassoADMM, LassoResult
+export fit_elbow, fit_elbow!
 
 function fit!(solver::LassoADMM, x::Vector, y::AbstractVector, A::AbstractMatrix, lambda::Real=1.0)
+    nx = length(x)
+    B = LinAlg.BLAS.gemm('T','N',1.0,A,A)
+    @inbounds for i in 1:nx
+        B[i,i] += solver.mu
+    end
+    cf = cholfact(Symmetric(B))
+    return fit_impl!(solver, x, y, A, cf, lambda)
+end
+
+function fit_elbow(solver::LassoADMM, y::AbstractVector, A::AbstractMatrix)
+    x = zeros(size(A,2))
+    return fit_elbow!(solver, x, y, A)
+end
+
+function fit_elbow!(solver::LassoADMM, x::Vector, y::AbstractVector, A::AbstractMatrix)
+    nx = length(x)
+    B = LinAlg.BLAS.gemm('T','N',1.0,A,A)
+    @inbounds for i in 1:nx
+        B[i,i] += solver.mu
+    end
+    cf = cholfact(Symmetric(B))
+
+    N = length(y)
+    K = length(x)
+    invK = 1.0/K
+
+    Ax = zeros(N)
+
+    lambda = 1.0
+    res = 0.0
+    nonzero = 0
+
+    fit_impl!(solver, x, y, A, cf, lambda)
+    LinAlg.BLAS.gemv!('N', 1.0, A, x, 0.0, Ax)
+    @inbounds for i in 1:N
+        res += (y[i]-Ax[i])^2
+    end
+    @inbounds for i in 1:K
+        nonzero += ifelse(x[i]==0.0, 0.0, 1)
+    end
+
+    lambdas = [lambda]
+    residues = [res]
+    nonzeros = [nonzero]
+
+    while nonzero > 0 && lambda < 1.0e8
+        lambda *= 2.0
+        fit_impl!(solver, x, y, A, cf, lambda)
+        LinAlg.BLAS.gemv!('N', 1.0, A, x, 0.0, Ax)
+        res = 0.0
+        nonzero = 0
+        @inbounds for i in 1:N
+            res += (y[i]-Ax[i])^2
+        end
+        @inbounds for i in 1:K
+            nonzero += ifelse(x[i]==0.0, 0.0, 1)
+        end
+        push!(lambdas, lambda)
+        push!(residues, res)
+        push!(nonzeros, nonzero)
+    end
+
+    high_lambda = lambdas[end]
+    high_res = residues[end]
+    lambda = lambdas[1]
+
+    while nonzero < K && lambda > 1.0e-8
+        lambda *= 0.5
+        fit_impl!(solver, x, y, A, cf, lambda)
+        LinAlg.BLAS.gemv!('N', 1.0, A, x, 0.0, Ax)
+        res = 0.0
+        nonzero = 0
+        @inbounds for i in 1:N
+            res += (y[i]-Ax[i])^2
+        end
+        @inbounds for i in 1:K
+            nonzero += ifelse(x[i]==0.0, 0.0, 1)
+        end
+        if(nonzero == 0)
+            high_lambda = lambdas[end] = lambda
+            high_res = residues[end] = res
+            nonzeros[end] = nonzero
+        else
+            push!(lambdas, lambda)
+            push!(residues, res)
+            push!(nonzeros, nonzero)
+        end
+    end
+    high_lambda = log(high_lambda)
+    low_lambda = log(lambdas[end])
+    low_res = residues[end]
+    slope = (high_res-low_res)/(high_lambda-low_lambda)
+
+    @show high_res, low_res, high_lambda, low_lambda,slope
+
+    f = x->low_res + slope*(log(x)-low_lambda)
+
+    max_diff = 0.0
+    imax = 1
+    for i in 1:length(lambdas)
+        res = f(lambdas[i])
+        diff = res - residues[i]
+        if diff > max_diff
+            max_diff = diff
+            imax = i
+        end
+    end
+    result = LassoResult(lambdas[imax], lambdas, residues, nonzeros)
+
+    return fit_impl!(solver, x, y, A, cf, lambdas[imax]), result
+end
+
+function fit_impl!(solver::LassoADMM, x::Vector, y::AbstractVector, A::AbstractMatrix, cf::LinAlg.Cholesky, lambda::Real)
     mu = solver.mu
     tol = solver.tol
     maxiter = solver.maxiter
     solver.costs = zeros(0)
+    stcoeff = lambda/solver.mu
 
     nx = length(x)
     ny = length(y)
 
+    invnx = 1.0/nx
     invmu = 1.0/mu
-    invlambda = 1.0/lambda
 
     ATy = LinAlg.BLAS.gemv('T', A, y)
     z = similar(x)
@@ -33,16 +155,11 @@ function fit!(solver::LassoADMM, x::Vector, y::AbstractVector, A::AbstractMatrix
     cost = 0.0
     LinAlg.BLAS.gemv!('N', 1.0, A, x, 0.0, Ax)
     @inbounds for i in 1:nx
-        cost += 0.5*invlambda*(y[i] - Ax[i])^2
-        cost += abs(x[i])
+        cost += 0.5*(y[i] - Ax[i])^2
+        cost += lambda*abs(x[i])
     end
-    push!(solver.costs, cost)
-
-    B = LinAlg.BLAS.gemm('T','N',invlambda,A,A)
-    @inbounds for i in 1:nx
-        B[i,i] += mu
-    end
-    cf = cholfact(Symmetric(B))
+    cost *= invnx
+    old_cost = cost
 
     for iter in 1:maxiter
         @inbounds for i in 1:nx
@@ -50,22 +167,24 @@ function fit!(solver::LassoADMM, x::Vector, y::AbstractVector, A::AbstractMatrix
         end
         A_ldiv_B!(cf, x)
         @inbounds for i in 1:nx
-            z[i] = soft_threshold(x[i]+invmu*h[i], invmu)
+            z[i] = soft_threshold(x[i]+invmu*h[i], stcoeff)
             h[i] += mu*(x[i]-z[i])
         end
 
         cost = 0.0
         LinAlg.BLAS.gemv!('N', 1.0, A, x, 0.0, Ax)
         @inbounds for i in 1:nx
-            cost += 0.5*invlambda*(y[i] - Ax[i])^2
-            cost += abs(x[i])
+            cost += 0.5*(y[i] - Ax[i])^2
+            cost += lambda*abs(x[i])
         end
+        cost *= invnx
         push!(solver.costs, cost)
-        if abs(cost - solver.costs[end-1])/cost < tol
+        if abs(cost - old_cost)/cost < tol
             break
         end
     end
     x[:] = z[:]
     return x
 end
+
 
